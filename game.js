3b97d9b1a306e2a51efc16595d8244654cbe4bfa
GangPil Song <92740959+strongfeel@users.noreply.github.com>
2024-08-21 17:22:55 +0900

    필수기능 구현

--- game.js
+++ game.js
@@ -1,23 +1,27 @@
-import chalk from "chalk";
-import readlineSync from "readline-sync";
+import chalk from 'chalk';
+import readlineSync from 'readline-sync';
 
 class Player {
   constructor() {
     this.hp = 100;
+    this.atk = 10;
   }
 
   attack() {
     // 플레이어의 공격
+    return chalk.red(`몬스터에게 ${this.atk}의 피해를 입혔습니다.`);
   }
 }
 
 class Monster {
-  constructor() {
-    this.hp = 100;
+  constructor(stage) {
+    this.hp = 20 + stage;
+    this.atk = 10 + stage;
   }
 
   attack() {
     // 몬스터의 공격
+    return chalk.red(`당신은 ${this.atk}의 피해를 입었습니다.`);
   }
 }
 
@@ -25,14 +29,21 @@ function displayStatus(stage, player, monster) {
   console.log(chalk.magentaBright(`\n=== Current Status ===`));
   console.log(
     chalk.cyanBright(`| Stage: ${stage} `) +
-      chalk.blueBright(`| 플레이어 정보`) +
-      chalk.redBright(`| 몬스터 정보 |`)
+      chalk.blueBright(`| Player HP : ${player.hp}, Attack: ${player.atk}`) +
+      chalk.redBright(`| Monster HP : ${monster.hp}, Attack: ${monster.atk} |`),
   );
   console.log(chalk.magentaBright(`=====================\n`));
 }
 
+// 시간 늦추기
+function sleep(ms) {
+  const wakeUpTime = Date.now() + ms;
+  while (Date.now() < wakeUpTime) {}
+}
+
 const battle = async (stage, player, monster) => {
   let logs = [];
+  let cnt = 0;
 
   while (player.hp > 0) {
     console.clear();
@@ -40,11 +51,37 @@ const battle = async (stage, player, monster) => {
 
     logs.forEach((log) => console.log(log));
 
-    console.log(chalk.green(`\n1. 공격한다 2. 아무것도 하지않는다.`));
-    const choice = readlineSync.question("당신의 선택은? ");
+    console.log(chalk.green(`\n1. 공격한다 2. 도망간다.`));
+    const choice = readlineSync.question('당신의 선택은? ');
 
     // 플레이어의 선택에 따라 다음 행동 처리
     logs.push(chalk.green(`${choice}를 선택하셨습니다.`));
+
+    if (choice === '1') {
+      cnt++;
+      logs.push(`[${cnt}]` + player.attack());
+      monster.hp -= player.atk;
+      if (monster.hp > 0) {
+        logs.push(`[${cnt}]` + monster.attack());
+        player.hp -= monster.atk;
+      } else {
+        logs.push(`[${cnt}]` + chalk.blue('축하합니다! 몬스터를 쓰러트렸습니다!'));
+        player.hp = 100;
+        sleep(1000);
+        break;
+      }
+    } else if (choice === '2') {
+      logs.push(`[${cnt}]` + chalk.yellow('싸움에서 도망을 쳤습니다.'));
+      sleep(1000);
+      break;
+    } else {
+      logs.push(chalk.green('정확한 번호를 입력해 주세요'));
+    }
+
+    // 스테이지 클리어 및 게임 종료 조건
+    if (player.hp <= 0) {
+      console.log('플레이어가 사망했습니다.');
+    }
   }
 };
 
@@ -57,8 +94,11 @@ export async function startGame() {
     const monster = new Monster(stage);
     await battle(stage, player, monster);
 
-    // 스테이지 클리어 및 게임 종료 조건
-
     stage++;
+  }
+
+  // 스테이지 클리어 및 게임 종료 조건
+  if (stage > 10 && player.hp > 0) {
+    console.log('모든 스테이지를 완료하였습니다.');
   }
 }
